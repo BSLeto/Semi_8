a7b390529d093583a60796028a64593769f319e6
Leto <mikhaylichenko2000@bk.ru>
2022-09-01 14:23:59 +0300

    выполнил задачу 62

---
+++ WH_62/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Write($"Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] spiralMatrix = new int[rows, columns];
+
+int temp = 1;
+int j = 0;
+int i = 0;
+
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+    spiralMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1) j++;
+    else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1) i++;
+    else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1) j--;
+    else i--;
+
+}
+
+void PrintArray(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(spiralMatrix);
